ff6767a8be5eea09d0b61bc2c827479e6d08201e
A-str0 <il.eroxin@gmail.com>
2025-04-16 13:23:33 +0300

    `Some tweaks`

--- Assets/AudioSystem/AudioSystemManager.cs
+++ Assets/AudioSystem/AudioSystemManager.cs
@@ -4,11 +4,12 @@ namespace AudioSystem
 {
     public class AudioSystemManager 
     {
-        public static void PlayClip(AudioEmitterSettings settings, Vector3 position = default)
+        public static void PlayClip(AudioEmitterSettings settings, Vector3 position = default, Transform parent = default)
         {
             GameObject obj = new GameObject();
             obj.name = $"{settings.Clip.name} clip emitter";
             obj.transform.position = position;
+            obj.transform.SetParent(parent);
 
             AudioEmitter emitter = obj.AddComponent<AudioEmitter>();
             emitter.Configure(settings);
@@ -16,15 +17,15 @@ namespace AudioSystem
         }
 
         /// <summary>
-        /// Overload for simple AudioClip play with default settings
+        /// Overload for simple AudioClip play with default AudioSource settings
         /// </summary>
         /// <param name="clip">Clip to play</param>
         /// <param name="position">AudioSource position in 3D</param>
-        public static void PlayClip(AudioClip clip, Vector3 position = default)
+        public static void PlayClip(AudioClip clip, Vector3 position = default, Transform parent = default)
         {
             AudioEmitterSettings defaultSettings = ScriptableObject.CreateInstance<AudioEmitterSettings>();
             defaultSettings.GetType().GetField("_clip", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance).SetValue(defaultSettings, clip);
-            PlayClip(defaultSettings, position);
+            PlayClip(defaultSettings, position, parent);
         }
     }
 }
