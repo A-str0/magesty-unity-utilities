28a253d7e65591b6a59920dc12e42144076ab320
A-str0 <il.eroxin@gmail.com>
2025-06-08 18:01:39 +0300

    feat(AudioNamespace): added AudioNamespace for spliting different sounds (ui, world, music) onto different tracks

---
+++ Assets/AudioSystem/AudioNamespace.cs
@@ -0,0 +1,58 @@
+using System;
+using UnityEngine;
+using UnityEngine.Pool;
+
+namespace AudioSystem
+{
+    [Serializable]
+    public class AudioNamespace
+    {
+        private ObjectPool<AudioEmitter> _objectsPool;
+
+        public AudioNamespace(int maxPoolCapacity, int defaultPoolCapacity)
+        {
+            _objectsPool = new ObjectPool<AudioEmitter>(
+                createFunc: CreateEmitter,
+                actionOnGet: OnGetEmitter,
+                actionOnRelease: OnReleaseEmitter,
+                actionOnDestroy: OnDestroyEmitter,
+                defaultCapacity: defaultPoolCapacity,
+                maxSize: maxPoolCapacity
+            );
+        }
+
+        public AudioEmitter Get()
+        {
+            AudioEmitter emitter = _objectsPool.Get();
+            emitter.SetParentThread(this);
+            return emitter;
+        }
+
+        public void Release(AudioEmitter emitter)
+        {
+            _objectsPool.Release(emitter);
+        }
+
+        private AudioEmitter CreateEmitter()
+        {
+            GameObject obj = new GameObject("AudioEmitter");
+            AudioEmitter emitter = obj.AddComponent<AudioEmitter>();
+            return emitter;
+        }
+
+        private void OnGetEmitter(AudioEmitter obj)
+        {
+            obj.gameObject.SetActive(true);
+        }
+
+        private void OnReleaseEmitter(AudioEmitter obj)
+        {
+            obj.gameObject.SetActive(false);
+        }
+
+        private void OnDestroyEmitter(AudioEmitter obj)
+        {
+            Debug.Log($"{this} Object pool is overloaded");
+        }
+    }
+}
